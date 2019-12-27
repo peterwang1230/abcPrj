010667ae003ad273941e5c4d274695cd82efeb43
Your Name <peterwang1230@gmail.com>
2019-12-27 23:36:02 +0800

    Add adjust weight function

--- a7_jdc_adj_weight.py
+++ a7_jdc_adj_weight.py
@@ -8,21 +8,6 @@ def adj_weight(key, filename='userdict_tw.txt'):
         val = 0.3
     return val
 
-# import pickle
-# pickle_in = open("jtitle_pickle", "rb")
-# df = pickle.load(pickle_in)
-# pickle_in.close()
-# def adj_weight2(key, df):
-#     s_title = set([])
-#     for i in df['JTITLE']:
-#         y = i.split()    # to list
-#         s_title.update(y)
-#     # print(s_title)
-#     val = 0
-#     if key in s_title:
-#         val = 0.3
-#     return val
-
 if __name__ == "__main__":
     print(adj_weight("毒品危害"))   # The main function goes here.
 
